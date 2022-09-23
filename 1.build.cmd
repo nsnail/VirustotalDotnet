5a2747397e94333ce77bc57c34bd11dfdaa8ca30
nsnail <taokeu@gmail.com>
2022-09-23 15:06:46 +0800

    自动下载编译环境

--- 1.build.cmd
+++ 1.build.cmd
@@ -1,3 +1,12 @@
+echo off
+dotnet --version
+if %errorlevel% == 0 (goto build)
+.\res\tools\7za.exe x -y -o.\res\tools\axel .\res\tools\axel.7z
+mkdir .\res\dotnet-sdk
+.\res\tools\axel\axel.exe -o .\res\dotnet-sdk\dotnet-sdk-7.0.100-rc.1.22431.12-win-x64.zip https://download.visualstudio.microsoft.com/download/pr/b3b5dce4-d810-4477-a8a3-97cbb0bdf3ea/91d0dd167239cfdfb48ae18166f444d4/dotnet-sdk-7.0.100-rc.1.22431.12-win-x64.zip
+if not exist .\res\dotnet-sdk\dotnet.exe (.\res\tools\7za.exe x -y -o.\res\dotnet-sdk .\res\dotnet-sdk\dotnet-sdk-7.0.100-rc.1.22431.12-win-x64.zip)
+set PATH=%PATH%;%CD%\res\dotnet-sdk
+:build
 dotnet new console -n VirustotalDotnet -o .\src\VirustotalDotnet
 copy .\src\Program.cs ".\src\VirustotalDotnet\Program.cs"
 dotnet add .\src\VirustotalDotnet\VirustotalDotnet.csproj package CommandLineParser
